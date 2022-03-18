392460740a23be62ffa9cf5290f3e8cbb8174510
박수근 <bwmelon97@naver.com>
2022-03-18 12:58:52 +0900

    feat: make skeleton code - (bg_10828_sg)

--- common/자료구조/backjoon_10828_soogeun/src/App.java
+++ common/자료구조/backjoon_10828_soogeun/src/App.java
@@ -1,5 +1,47 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    private int size = 0;
+    private StackItem topItem;
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    private int push(int input) {
+        return 0;
+    }
+
+    private int pop() {
+        return 0;
+    }
+
+    private int size() {
+        return 0;
+    }
+
+    private boolean empty() {
+        return false;
+    }
+
+    private int top() {
+        return 0;
+    }
+}
+
+
+class StackItem {
+    private int value;
+    private StackItem prevItem;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void pushItem(int value, StackItem prevItem) {
+    }
+
+    public StackItem popItem() {
+        return prevItem;
     }
 }
