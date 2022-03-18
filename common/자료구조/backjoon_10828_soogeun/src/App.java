7fb550ee4a1acb79f668278f6c7e4901bf13d776
박수근 <bwmelon97@naver.com>
2022-03-18 13:45:31 +0900

    feat: [bj_10828_sg] - implement input command

--- common/자료구조/backjoon_10828_soogeun/src/App.java
+++ common/자료구조/backjoon_10828_soogeun/src/App.java
@@ -1,11 +1,42 @@
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class App {
 
     private int size = 0;
     private StackItem topItem;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int lineCounts = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < lineCounts; i++) {
+            String command = br.readLine();
+            String[] argments = command.split(" ");
+
+            switch (argments[0]) {
+                case "push":
+                    int pushItem = Integer.parseInt(argments[1]);
+                    System.out.println("command is push + " + pushItem);
+                    break;
+                case "pop":
+                    System.out.println("command is pop");
+                    break;
+                case "size":
+                    System.out.println("command is size");
+                    break;
+                case "empty":
+                    System.out.println("command is empty");
+                    break;
+                case "top":
+                    System.out.println("command is top");
+                    break;
+                default:
+                    System.out.println("Wrong command");
+            }
+        }
+
+        br.close();
     }
 
     private int push(int input) {
