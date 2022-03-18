9c6ad93720d44a819550653b1a0e492f5d554a61
박수근 <bwmelon97@naver.com>
2022-03-18 13:48:31 +0900

    feat: [bj_10828_sg] - devide stack logic
    - Stack 관련 로직을 App class에서 Stack Class로 분리

--- common/자료구조/backjoon_10828_soogeun/src/App.java
+++ common/자료구조/backjoon_10828_soogeun/src/App.java
@@ -3,12 +3,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class App {
-
-    private int size = 0;
-    private StackItem topItem;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int lineCounts = Integer.parseInt(br.readLine());
+
+        Stack stack = new Stack();
 
         for (int i = 0; i < lineCounts; i++) {
             String command = br.readLine();
@@ -17,19 +16,19 @@ public class App {
             switch (argments[0]) {
                 case "push":
                     int pushItem = Integer.parseInt(argments[1]);
-                    System.out.println("command is push + " + pushItem);
+                    stack.push(pushItem);
                     break;
                 case "pop":
-                    System.out.println("command is pop");
+                    System.out.println( stack.pop() );
                     break;
                 case "size":
-                    System.out.println("command is size");
+                    System.out.println( stack.size() );
                     break;
                 case "empty":
-                    System.out.println("command is empty");
+                    System.out.println( stack.empty() );
                     break;
                 case "top":
-                    System.out.println("command is top");
+                    System.out.println( stack.top() );
                     break;
                 default:
                     System.out.println("Wrong command");
@@ -38,28 +37,36 @@ public class App {
 
         br.close();
     }
-
-    private int push(int input) {
-        return 0;
-    }
-
-    private int pop() {
-        return 0;
-    }
-
-    private int size() {
-        return 0;
-    }
-
-    private boolean empty() {
-        return false;
-    }
-
-    private int top() {
-        return 0;
-    }
 }
 
+class Stack {
+    private int size;
+    private StackItem topItem;
+
+    public Stack() {
+        this.size = 0;
+        this.topItem = null;
+    }
+
+    public void push(int input) {
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int size() {
+        return 1;
+    }
+
+    public int empty() {
+        return 2;
+    }
+
+    public int top() {
+        return 3;
+    }
+}
 
 class StackItem {
     private int value;
