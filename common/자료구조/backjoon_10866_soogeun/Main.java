8e2f2e827e0c95bfcb8257da2eb16d6a1e949db6
Soogeun Park <bwmelon97@naver.com>
2022-03-30 00:07:47 +0900

    feat: implement deque class (solve bj_10866)

--- common/자료구조/backjoon_10866_soogeun/Main.java
+++ common/자료구조/backjoon_10866_soogeun/Main.java
@@ -6,33 +6,34 @@ public class Main {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int numsOfCommandLines = Integer.parseInt(br.readLine());
+        SooDequq dequq = new SooDequq();
 
         for (int i = 0; i < numsOfCommandLines; i++) {
             String command = br.readLine();
 
             switch (command) {
                 case Constants.POP_FRONT:
-                    System.out.println(command);
+                    System.out.println(dequq.popFront());
                     break;
 
                 case Constants.POP_BACK:
-                    System.out.println(command);
+                    System.out.println(dequq.popBack());
                     break;
 
                 case Constants.SIZE:
-                    System.out.println(command);
+                    System.out.println(dequq.size());
                     break;
 
                 case Constants.EMPTY:
-                    System.out.println(command);
+                    System.out.println(dequq.empty() ? "1" : "0");
                     break;
 
                 case Constants.FRONT:
-                    System.out.println(command);
+                    System.out.println(dequq.front());
                     break;
 
                 case Constants.BACK:
-                    System.out.println(command);
+                    System.out.println(dequq.back());
                     break;
 
                 /**
@@ -43,13 +44,13 @@ public class Main {
                     assert arguments.length == 2: Constants.COMMAND_ERROR;
                     
                     command = arguments[0];
-                    int InputValue = Integer.parseInt(arguments[1]);
+                    int inputValue = Integer.parseInt(arguments[1]);
 
                     if ( command.equals(Constants.PUSH_FRONT) ) {
-                        System.out.println(command + InputValue);
+                        dequq.pushFront(inputValue);
                     }
                     else if ( command.equals(Constants.PUSH_BACK) ) {
-                        System.out.println(command + InputValue);
+                        dequq.pushBack(inputValue);
                     }
                     else 
                         throw new Error(Constants.COMMAND_ERROR);
@@ -60,22 +61,79 @@ public class Main {
     }    
 }
 
-class SooDequq <T> {
-    private SooItem<T> head;
-    private SooItem<T> tail;
+/**
+ * Generics로 구현하려 했으나, empty일 때 -1을 출력하게끔 반환하는 부분에서 막혀서 구현하지 못함.
+ */
+class SooDequq {
+    private SooItem head;
+    private SooItem tail;
     private int size;
 
     public SooDequq() {
+        this.initiateDeque();
+    }
+
+    private void initiateDeque() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
 
-    public void pushFront(T inputValue) {}
-    public void pushBack(T inputValue) {}
-    public T popFront() {
+    public void pushFront(int inputValue) {
+        SooItem newItem = new SooItem(inputValue, this.head, null);
+        if (this.empty()) 
+            this.tail = newItem;
+        else 
+            this.head.next = newItem;
+        this.head = newItem;
+        this.size += 1;
     }
-    public T popBack() {}
+
+    public void pushBack(int inputValue) {
+        SooItem newItem = new SooItem(inputValue, null, this.tail);
+        if (this.empty()) 
+            this.head = newItem;
+        else
+            this.tail.prev = newItem;
+        this.tail = newItem;
+        this.size += 1;
+    }
+
+    public int popFront() {
+        if (this.empty())
+            return -1;
+
+        int returnValue = this.head.getValue();
+
+        if (this.size == 1) {
+            this.initiateDeque();
+        }
+        else {
+            this.head = this.head.prev;
+            this.head.next = null;
+            this.size -= 1;
+        }
+
+        return returnValue;
+    }
+
+    public int popBack() {
+        if (this.empty())
+            return -1;
+
+        int returnValue = this.tail.getValue();
+        
+        if (this.size == 1) {
+            this.initiateDeque();
+        }
+        else {
+            this.tail = this.tail.next;
+            this.tail.prev = null;
+            this.size -= 1;
+        }
+
+        return returnValue;
+    }
 
     public int size() { 
         return this.size; 
@@ -85,11 +143,16 @@ class SooDequq <T> {
         return this.size == 0; 
     }
 
-    public T front() {}
-    public T back() {}
+    public int front() {
+        return this.empty() ? -1 : this.head.getValue();
+    }
+
+    public int back() {
+        return this.empty() ? -1 : this.tail.getValue();
+    }
 }
 
-class SooItem <T> {
+class SooItem {
     /**
      * prev와 next를 default로 선언한 이유는, 해당 property들이 getter, setter 
      * 모두 정의 되어야 하며 이 두 method 외에는 사용되는 경우가 없기 때문에, default로
@@ -98,17 +161,17 @@ class SooItem <T> {
      * 변경될 때, Deque 전체에 영향을 주기 때문에, 같은 패키지 내에서만 접근을 허용할 수 있도록
      * public이 아닌 default로 선언하였습니다.  
      */
-    SooItem<T> prev;
-    SooItem<T> next;
-    private T value;
+    SooItem prev;
+    SooItem next;
+    private int value;
 
-    public SooItem(T value, SooItem prev, SooItem next) {
+    public SooItem(int value, SooItem prev, SooItem next) {
         this.value = value;
         this.prev = prev;
         this.next = next;
     }
 
-    public T getValue() {
+    public int getValue() {
         return value;
     }
 }
