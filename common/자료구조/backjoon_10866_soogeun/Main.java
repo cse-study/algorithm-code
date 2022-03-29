49789dda57b6f6cdcd2196c02d545731d3938975
Soogeun Park <bwmelon97@naver.com>
2022-03-29 20:10:11 +0900

    feat: design classes of bj-10866
    - Dequq 클래스와, Deqeq Item 클래스의 property와 method 정의
    - 기능은 구현되지 않았으며, return 값이 존재하지 않기 때문에 method에서 컴파일 에러가
      발생합니다.

--- common/자료구조/backjoon_10866_soogeun/Main.java
+++ common/자료구조/backjoon_10866_soogeun/Main.java
@@ -60,6 +60,59 @@ public class Main {
     }    
 }
 
+class SooDequq <T> {
+    private SooItem<T> head;
+    private SooItem<T> tail;
+    private int size;
+
+    public SooDequq() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void pushFront(T inputValue) {}
+    public void pushBack(T inputValue) {}
+    public T popFront() {
+    }
+    public T popBack() {}
+
+    public int size() { 
+        return this.size; 
+    }
+
+    public boolean empty() { 
+        return this.size == 0; 
+    }
+
+    public T front() {}
+    public T back() {}
+}
+
+class SooItem <T> {
+    /**
+     * prev와 next를 default로 선언한 이유는, 해당 property들이 getter, setter 
+     * 모두 정의 되어야 하며 이 두 method 외에는 사용되는 경우가 없기 때문에, default로
+     * 선언했을 때와 차이가 없기 때문입니다.
+     * 따라서 코드의 가독성을 높이기 위해 default로 선언하였습니다. 하지만, 두 property 모두 
+     * 변경될 때, Deque 전체에 영향을 주기 때문에, 같은 패키지 내에서만 접근을 허용할 수 있도록
+     * public이 아닌 default로 선언하였습니다.  
+     */
+    SooItem<T> prev;
+    SooItem<T> next;
+    private T value;
+
+    public SooItem(T value, SooItem prev, SooItem next) {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
+
 
 /**
  * class that defines the constants.
