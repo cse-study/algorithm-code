2527ee5ac9e1078a4785d3af16fd48c22ec6b94d
Yuho Jeong <yuho8437@unist.ac.kr>
2022-04-03 15:57:35 +0900

    feat: leetcode 509 fibonacci

---
+++ common/dynamic_programming/leetcode_509_yuho.py
@@ -0,0 +1,14 @@
+"""
+LeetCode 509. Fibonacci Number
+https://leetcode.com/problems/fibonacci-number/
+"""
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)
