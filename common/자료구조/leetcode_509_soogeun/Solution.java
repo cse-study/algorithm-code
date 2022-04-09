4fa1dc33de00fc20c942352fc480f6db2d988b2c
Soogeun Park <bwmelon97@naver.com>
2022-04-09 18:35:04 +0900

    feat: lc-509 solve fibonacci with solution array
    - O(n^2)의 시간 복잡성을 O(n)으로 줄임

--- common/자료구조/leetcode_509_soogeun/Solution.java
+++ common/자료구조/leetcode_509_soogeun/Solution.java
@@ -9,4 +9,19 @@ public class Solution {
         if (n == 1) return 1;
         return fib_recurrsive(n - 1) + fib_recurrsive(n - 2);
     }
+
+    public int fib_array(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int[] solArr = new int[n + 1];
+        solArr[0] = 0;
+        solArr[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            solArr[i] = solArr[i - 1] + solArr[i - 2];
+        }
+
+        return solArr[n];
+    }
 }
