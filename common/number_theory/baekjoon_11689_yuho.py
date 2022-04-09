0e055e1754cb62aa2c33d1c268bb3d74683b2b7c
Yuho Jeong <yuho8437@unist.ac.kr>
2022-04-09 15:52:02 +0900

    fix: 변수명 수정
    기존 remainder 라는 변수명이 '나머지'로 오해가 생길 듯 하여 수정

--- common/number_theory/baekjoon_11689_yuho.py
+++ common/number_theory/baekjoon_11689_yuho.py
@@ -6,20 +6,20 @@ https://www.acmicpc.net/problem/11689
 
 class EulerPhi(object):
     def __init__(self, n):
-        self.n = n              # 입력 n
-        self.remainder = n      # n의 약수로 계속해서 나눠 줄 변수
-        self.result = n         # 오일러파이 결과
+        self.n = n          # 입력 n
+        self.temp = n       # n의 약수로 계속해서 나눠 줄 변수
+        self.result = n     # 오일러파이 결과
 
     def run(self):
         for i in range(2, int(self.n ** 0.5) + 1):
-            if self.remainder % i == 0:             # 소인수 분해
-                self.result *= 1 - (1 / i)          # 오일러파이 식 적용
-                while self.remainder % i == 0:      # 중복 소인수 제거
-                    self.remainder = self.remainder // i
+            if self.temp % i == 0:              # 소인수 분해
+                self.result *= 1 - (1 / i)      # 오일러파이 식 적용
+                while self.temp % i == 0:       # 중복 소인수 제거
+                    self.temp = self.temp // i
 
         # n이 소수인 경우
-        if self.remainder != 1:
-            self.result *= 1 - (1 / self.remainder)
+        if self.temp != 1:
+            self.result *= 1 - (1 / self.temp)
 
         return int(self.result)
 
