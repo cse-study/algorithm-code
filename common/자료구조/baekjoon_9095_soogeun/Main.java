7ed442cc0d51f019576f8b12cd8d6fd45c6ba508
Soogeun Park <bwmelon97@naver.com>
2022-04-09 17:18:41 +0900

    feat: bj-9095 implement main logic

--- common/자료구조/baekjoon_9095_soogeun/Main.java
+++ common/자료구조/baekjoon_9095_soogeun/Main.java
@@ -5,8 +5,20 @@
 
 package baekjoon_9095;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numOfLines = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < numOfLines; i++) {
+            int input = Integer.parseInt(br.readLine());
+            System.out.println(input);
+        }
+
+        br.close();
     }
 }
