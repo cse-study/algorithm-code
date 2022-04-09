6f00f66bab13dc0a331a4c72aafbc6c1895511c8
Soogeun Park <bwmelon97@naver.com>
2022-04-09 17:11:00 +0900

    feat: bj-9095 start to slove baekjoon 9095

---
+++ common/자료구조/baekjoon_9095_soogeun/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Baekjoon 9095 1,2,3 더하기
+ * https://www.acmicpc.net/problem/9095
+ */
+
+package baekjoon_9095;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
