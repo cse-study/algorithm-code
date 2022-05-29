89d81260c98bc9fa3486fbff988950b6cb6d0e97
Yuho Jeong <yuho8437@unist.ac.kr>
2022-05-29 12:51:21 +0900

    feat: leetcode 23 Merge k Sorted Lists

--- common/divide_and_conquer/leetcode_23_yuho/main.cpp
+++ common/divide_and_conquer/leetcode_23_yuho/main.cpp
@@ -8,8 +8,81 @@
 */
 
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    static ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+        auto* answer = new ListNode;
+        ListNode *cur_answer_node = answer;
+        bool done = false;
+
+        // Only consider the smallest element of each list (= search_list)
+        std::vector<ListNode*> search_list;
+        for (ListNode *min_node: lists) {
+            // Exception for "[[]]" case
+            if (min_node != nullptr) {
+                search_list.push_back(min_node);
+            }
+        }
+
+        // Check the length of search_list
+        // Exception for "[]" case
+        if (search_list.empty()) {
+            answer = nullptr;
+            done = true;
+        }
+
+        while (!done) {
+            int min_val = 1e+5;
+            int min_index = -1;
+
+            // Find the minimum value in search list.
+            for (int i = 0; i < search_list.size(); i ++){
+                ListNode *cur_search_node = search_list[i];
+
+                if (cur_search_node != nullptr) {
+                    if (min_val > cur_search_node->val) {
+                        min_val = cur_search_node->val;
+                        min_index = i;
+                    }
+                }
+            }
+
+            // There is no assigned minimum value. All search nodes are nullptr.
+            if (min_val == 1e+5 and min_index == -1) {
+                done = true;
+            }
+            else {
+                cur_answer_node->val = min_val;
+                search_list[min_index] = search_list[min_index]->next;
+
+                // Check whether search_list has only nullptr
+                bool all_null = true;
+                for (auto & node : search_list) {
+                    if (node != nullptr) {
+                        all_null = false;
+                        break;
+                    }
+                }
+
+                if (!all_null) {
+                    cur_answer_node->next = new ListNode;
+                    cur_answer_node = cur_answer_node->next;
+                }
+            }
+        }
+        return answer;
+    }
+};
+
