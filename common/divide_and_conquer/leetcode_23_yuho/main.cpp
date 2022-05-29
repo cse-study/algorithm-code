8ec13c433649666d5cddf942248d456e678167bc
Yuho Jeong <yuho8437@unist.ac.kr>
2022-05-29 11:30:16 +0900

    feat: programmers 입국심사

---
+++ common/divide_and_conquer/leetcode_23_yuho/main.cpp
@@ -0,0 +1,15 @@
+/*
+    Leetcode 23: Merge k Sorted Lists (https://leetcode.com/problems/merge-k-sorted-lists/)
+
+    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+    Merge all the linked-lists into one sorted linked-list and return it.
+
+    @author Yuho Jeong
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
